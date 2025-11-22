29c157582eafa55b92cb12b2c15cd19476a0add2
Marcos Jacobs <marcos.jacobs@remitian.com>
2025-11-22 14:42:33 -0300

    feat: configure SwaggerGen to use custom schema IDs for better type identification

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Progr
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => type.FullName);
+});
 
 var app = builder.Build();
 
