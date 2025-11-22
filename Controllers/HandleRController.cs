666ad70d0c684918f61228b95f00b0c6a83e9105
Marcos Jacobs <marcos.jacobs@remitian.com>
2025-11-22 14:46:10 -0300

    feat: add HandleRController for handling CreateItem commands

---
+++ Controllers/HandleRController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using SomeStuff.Application.Mediator.HandleR;
+using SomeStuff.Application.Mediator.HandleR.Commands;
+
+namespace SomeStuff.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HandleRController : ControllerBase
+{
+    public HandleRController() {}
+
+    [HttpPost]
+    public async Task<IActionResult> CreateItem([FromBody] CreateItemCommand command, CancellationToken cancellationToken)
+    {
+        var handler = new HandleR();
+        var result = await handler.Handle(command);
+        return Ok(result);
+    }
+}
