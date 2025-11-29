97bb046896f3b7f5e38bf654786aa2d09a7f4971
Marcos Jacobs <marcos.jacobs@remitian.com>
2025-11-29 14:24:14 -0300

    refactor: improve HandleR class structure and method implementation

--- Application/Mediator/HandleR/HandleR.cs
+++ Application/Mediator/HandleR/HandleR.cs
@@ -2,12 +2,25 @@ namespace SomeStuff.Application.Mediator.HandleR;
 
 public class HandleR : IHandleR
 {
+    private readonly IServiceProvider _serviceProvider;
+
+    public HandleR(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
     public Task<THandlerResponse> HandleAsync<THandlerResponse>(ICommand<THandlerResponse> command)
     {
-        // Needs to resolve tye type
-        // Find a the handler implementation for that type
-        // Execute that handler
+        // Needs to resolve the type
+        var commandType = command.GetType(); // it will return CreateItemCommand
 
-        return Task.FromResult<THandlerResponse>(default!);
+        // Find the handler implementation for that type
+        var handlerType = typeof(ICommandHandler<,>).MakeGenericType(commandType, typeof(THandlerResponse)); // it will return ICommandHandler<CreateItemCommand, ItemDto>
+        var handlerInstance = _serviceProvider.GetRequiredService(handlerType); // it will return instance of CreateItemCommandHandler
+        var handleMethod = handlerType.GetMethod(nameof(ICommandHandler<ICommand<THandlerResponse>, THandlerResponse>.HandleAsync));
+
+        // Execute that handler
+        var result = handleMethod?.Invoke(handlerInstance, [command]);
+        return (Task<THandlerResponse>)result!;
     }
 }
